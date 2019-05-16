2a1e0780577ddfe007140cd7e3622872e57f5e28
Elina Yon <elinayon@berkeley.edu>
2019-05-16 02:31:26 -0700

    training/testing split

--- svm/import_data.py
+++ svm/import_data.py
@@ -4,6 +4,9 @@ from peekable import Peekable
 DATASET_ROOT = "../dataset/"
 FILE_NAME = "simulation"
 
+global train_i
+global test_i
+
 def get_genotypes():
     genotype_file_loc = DATASET_ROOT + FILE_NAME + ".phgeno"
     genotype_file = Peekable(filename=genotype_file_loc)
@@ -19,8 +22,19 @@ def get_genotypes():
             indiviudals[i].append(int(line[i]))
 
     arr = np.array(indiviudals)
-    mapper = np.vectorize(lambda x: x * 2 - 1)
-    return mapper(arr)  # Super duper efficient way of converting 0 -> -1
+    # mapper = np.vectorize(lambda x: x * 2 - 1)
+    # return mapper(arr)  # Super duper efficient way of converting 0 -> -1
+    # print("ARR TESTING!" , arr[:10,])
+    training_i = np.random.randint(arr.shape[0], size=int(len(arr)*2/3))
+    # print(arr.shape, int(len(arr)*2/3))
+    testing_i = np.random.randint(arr.shape[0], size=int(len(arr)/3))
+    # print("TRAIN I:" , training_i)
+    global train_i, test_i
+    train_i = training_i
+    test_i = testing_i
+    training, test = arr[training_i,:], arr[test_i,:]
+    # print("TRAIN:", training[:10,], "TEST:", test[:10,])
+    return training, test
 
 def get_ancestry():
     ancestry_file_loc = DATASET_ROOT + FILE_NAME + ".ancestry"
@@ -28,14 +42,19 @@ def get_ancestry():
 
     first_line = ancestry_file.peek().replace('-','').strip()
 
-    indiviudals = [[] for _ in first_line]
+    individuals = [[] for _ in first_line]
 
     # print(indiviudals)
     for line in ancestry_file:
         line = line.strip().replace('-', '').replace('A', '0').replace('B', '1')
 
         for i in range(len(line)):
-            indiviudals[i].append(int(line[i]))
+            individuals[i].append(int(line[i]))
 
-    return np.array(indiviudals)
-
+    # return np.array(indiviudals)
+    arr = np.array(individuals)
+    # print("ANCESTRY ARR TESTING!" , arr[:10,], "ARR SHAPE", arr.shape)
+    # print("TRAIN I:" , train_i)
+    training, test = arr[train_i,:], arr[test_i,:]
+    # print("TRAINING", training.shape)
+    return training, test
--- svm/model.py
+++ svm/model.py
@@ -8,33 +8,62 @@ from import_data import *
 
 def augment_features(features, window_size=1):
     assert window_size & 1, "Window size must be odd"
+    # print(features.shape)
     num_sites = features.shape[1]
 
-    return (features[:, i - window_size//2 : i + 1 + window_size//2] for i in range(window_size//2, num_sites - window_size//2))
+    # return np.hstack( a[i:1+i-window_size or None:1] for i in range(0,window_size))
+
+    aug = (features[:, i - window_size//2 : i + 1 + window_size//2] for i in range(window_size//2, num_sites - window_size//2))
+
+    return aug
 
 
 def augment_labels(labels, window_size=1):
     assert window_size & 1, "Window size must be odd"
-    num_sites = features.shape[1]
+    num_sites = labels.shape[1]
 
     return (labels[:, i] for i in range(window_size//2, num_sites - window_size//2))
 
 
 w = 11
-features = get_genotypes()
-labels = get_ancestry()
+# features = get_genotypes()
+# labels = get_ancestry()
 
-labels[:,0:1000].shape
+train_features, test_features = get_genotypes()
+train_labels, test_labels = get_ancestry()
 
-print(features.shape)
-print(labels.shape)
+# labels[:,0:1000].shape
+#
+# train_labels[:,0:1000].shape
+# test_labels[:,0:1000].shape
+
+# print(features.shape)
+# print(labels.shape)
+
+print("FEATURES: TRAIN :" , train_features.shape,  "TEST :", test_features.shape )
+print("LABELS: TRAIN :" , train_labels.shape,  "TEST :", test_labels.shape )
 
 models = []
-for features, labels in zip(augment_features(features, window_size=w), augment_labels(labels, window_size=w)):
-    print("Features shape: %s \tLabels shape: %s" % (str(features.shape), str(labels.shape)))
+
+for features, labels in zip(augment_features(train_features, window_size=w), augment_labels(train_labels, window_size=w)):
+    # print("Features shape: %s \tLabels shape: %s" % (str(features.shape), str(labels.shape)))
+    # print(labels)
+    if labels.shape[0] <= 1:
+        pass
     model = Model(kernel="linear")
     model.fit(features, labels)
-    predictions = model.predict(features)
-    print(model.score(features, labels))
     models.append(model)
 
+print("Done training")
+scores = []
+
+i = 0
+for features, labels in zip(augment_features(test_features, window_size=w), augment_labels(test_labels, window_size=w)):
+    # print("Features shape: %s \tLabels shape: %s" % (str(features.shape), str(labels.shape)))
+    model = models[i]
+    predictions = model.predict(features)
+    # print("Score:", model.score(features, labels))
+    scores.append(model.score(features,labels))
+    i+=1
+
+print("Average score:", np.average(scores))
